65938618b27f903d88522a2437e498f191a61e7f
Suraches Seesat <suraches.see@ascendcorp.com>
2020-06-13 00:08:48 +0700

    remove awesome and some unuse component

--- src/Components/Themes/Truecoffee/Layout/index.tsx
+++ src/Components/Themes/Truecoffee/Layout/index.tsx
@@ -9,7 +9,7 @@ import { CategoryBar } from "../CategoryBar";
 import { Topbar } from "../Topbar";
 import { Modal } from "../Modal";
 import { Header } from "../Header";
-import { Footer } from "@Components/Footer";
+import { Footer } from "../Footer";
 // #endregion Local Imports
 
 const Layout: React.FunctionComponent<ILayout.IProps> = ({ children, title = 'True Coffee : Smart Ordering' }): JSX.Element => {
@@ -18,21 +18,11 @@ const Layout: React.FunctionComponent<ILayout.IProps> = ({ children, title = 'Tr
         <Header text={ title }/>
         <div className="loading-icon" />
         <div className="wrapper">
-          {/* Topbar */}
           <Topbar />
-          {/* End Topbar */}
-          {/* Categories */}
           <CategoryBar />
-          {/* End Categories */}
-          {/* Item List */}
           { children }
-          {/* End Item List */}
-          {/* Cart Bar */}
           <CartBar />
-          {/* End Cart Bar */}
-          {/* Modal */}
           <Modal />
-          {/* End Modal */}
         </div>
         <Footer />
       </div>
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,7 +1,2 @@
-export { Layout } from "@Components/Layout";
-export { Navbar } from "@Components/Navbar";
-export { Footer } from "@Components/Footer";
-export { Heading } from "@Components/Heading";
-export { LocaleButton } from "@Components/LocaleButton";
 export { MenuList } from "@Components/MenuList";
 export { SingleMenu } from "@Components/SingleMenu";
